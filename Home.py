2258ad1b7811012ae29bab6a8738c54f82a8799b
KEMAYOUFRANCK <pfranckemayou@gmail.com>
2024-12-31 18:36:41 +0100

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,28 @@
+import streamlit as st
+
+
+st.set_page_config(layout="wide")
+
+st.sidebar.title("Contacts")
+st.sidebar.info(
+    """
+    - ANABA OHANDZA Telesphore Rodrigue
+    - ANATO Diane
+    """
+)
+
+
+
+
+st.title("Capital humain et croissance économique en Côte d'Ivoire")
+
+st.markdown(
+    """
+    une approche par les équations simultanées
+
+    """
+)
+
+st.info("Cliquez sur le menu latéral de gauche pour naviguer vers les différentes applications.")
+
+st.subheader("Profitez de votre expérience!")
